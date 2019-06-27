50099329d04d08f90e71afd50e7238702f7a7087
very-amused <very-amused@pm.me>
2019-06-26 23:52:30 -0400

    Improve featured page script

---
+++ scripts/featuredPage.js
@@ -0,0 +1,25 @@
+const links = [
+    {
+        title: 'Upload',
+        url: 'https://apfs.xyz/upload'
+    },
+    {
+        title: 'Minecraft',
+        url: 'https://apfs.xyz/mc'
+    }
+    // Add more
+];
+
+function featuredPage() {
+    // Select a random subpage
+    let link = links[Math.floor(Math.random() * links.length)];
+
+    // Get elements by ID
+    let elements = {};
+    elements.featuredTitle = document.getElementById('featured-title');
+    elements.featuredLink = document.getElementById('featured-link');
+
+    // Set the content to the random subpage
+    elements.featuredTitle.innerHTML = link.title;
+    elements.featuredLink.href = link.url;
+}
