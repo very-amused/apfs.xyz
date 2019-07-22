a2ae3d82a580a9124a98602ac6576b1a8d2b87ea
very-amused <very-amused@pm.me>
2019-07-21 20:43:43 -0400

    Add token generation
    Confirmation tokens will be used with an API to confirm accounts

--- login/routes/create-account.js
+++ login/routes/create-account.js
@@ -11,21 +11,41 @@ async function genID() {
     return ID;
 }
 
+// Function to generate a random 20-character token
+async function genToken() {
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+    let token = '';
+    for (let i = 0; i < 20; i++) {
+        token += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return token;
+}
+
+// Function to select a column from an SQL table and convert it to an array
+async function selectAsArray(column, table, conn) {
+    let sqlColumn = await conn.query(`SELECT ${column} FROM ${table}`);
+    let dataArray = [];
+    sqlColumn.forEach(object => {
+        dataArray.push(object[column]);
+    }); // A forEach loop is used to convert the sql column to an array
+    return dataArray;
+}
+
 async function main(pool, email, password) {
     let conn = await pool.getConnection();
     console.log(`Connected, the connection ID is ${conn.threadId}`);
 
+    // Throw an error if the user already exists
+    let users = await selectAsArray('Email', 'Users', conn);
+    if (users.includes(email)) {
+        throw `A user with the email address of ${email} already exists.`;
+    } // Error for if the user already exists and has been confirmed (isn't in the tokens table)
+
     // Query ID column for currently existing IDs
-    let IDcolumn = await conn.query('SELECT ID FROM Users');
-    let IDs = [];
-    IDcolumn.forEach(object => {
-        IDs.push(object.ID);
-    }); // A forEach loop is used to convert the sql column to a list
+    let IDs = await selectAsArray('ID', 'Users', conn);
 
-    // Generate a random ID
+    // Generate a random, unique ID
     let ID = await genID();
-
-    // Regenerate the ID until it's unique
     while (IDs.includes(ID)) {
         ID = await genID();
     }
@@ -38,6 +58,19 @@ async function main(pool, email, password) {
     // Insert the info into the Users table
     conn.query('INSERT INTO Users (ID, Email, Password) VALUES (?, ?, ?)',
     [ID, email, hashedPassword]);
+
+    // Query Token column for currently existing tokens
+    let tokens = await selectAsArray('Token', 'Tokens', conn);
+    
+    // Generate a unique confirmation token for the new user
+    token = await genToken();
+    while (token in tokens) {
+        token = await genToken();
+    }
+
+    // Insert the info into the Tokens table
+    conn.query('INSERT INTO Tokens (Email, Token) VALUES (?, ?)',
+    [email, token]);
 }
 
 router.get('/', (req, res) => {
@@ -52,8 +85,14 @@ router.post('/', (req, res) => {
 
     // Insert user info into the db
     main(req.app.get('pool'), req.body.email, req.body.password)
-    .catch(err => res.render('creation-error', {err: err})); // Render error page if an error occurs
-    res.render('creation-success', {email: req.body.email});
+    .catch(err => {
+        if (err) {
+            res.render('creation-error', {err: err}); // Render error page if an error occurs
+        }
+        else {
+            res.render('creation-success', {email: req.body.email});
+        }
+    }); 
 });
 
 module.exports = router;
