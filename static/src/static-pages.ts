c6c5a39e2dafff347f7fa4aa43508aa04722742a
very-amused <very-amused@pm.me>
2019-09-15 14:58:44 -0400

    Automate rendering of static pages

--- static/src/static-pages.ts
+++ static/src/static-pages.ts
@@ -1,23 +1,31 @@
 // Initialize Express
-import express = require('express');
+import * as express from 'express';
 const app = express();
 const port = 3030;
 
 // Initialize pug for rendering templates
 app.set('view engine', 'pug');
-app.set('views', '../templates');
 
 // Serve static files (css, js, images) from the public directory
 app.use(express.static('../public'));
 
-// Render the pug homepage template
+// Render the homepage
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render(`${__dirname}/../templates/home`);
 });
 
-// Render the Minecraft page
-app.get('/mc', (req, res) => {
-    res.render('mc');
+// Render each index.pug
+import * as fs from 'fs';
+fs.readdir('../templates', (err, files) => {
+    files.forEach(page => {
+        const path = `${__dirname}/../templates/${page}`;
+        if (!fs.lstatSync(path).isDirectory()) {
+            return;
+        }
+        app.get(`/pages/${page}`, (req, res) => {
+            res.render(`${path}/index`);
+        });
+    });
 });
 
 app.listen(port, () => {
