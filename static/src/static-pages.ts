d1b586de132a0e0ba01ddb325451a20c3f44bb71
very-amused <very-amused@pm.me>
2019-09-26 13:58:09 -0400

    Add function for recursively scanning and rendering pug templates
    This function allows templates to be placed in subdirectories, which will correspond to subroutes on the website. For example , templates/accounts/create-account/index.pug will render at /accounts/create-account

--- static/src/static-pages.ts
+++ static/src/static-pages.ts
@@ -9,25 +9,37 @@ app.set('view engine', 'pug');
 // Serve static files (css, js, images) from the public directory
 app.use(express.static('../public'));
 
-// Render the homepage
-app.get('/', (req, res) => {
-    res.render(`${__dirname}/../templates/home`);
-});
-
-
 // Render each directory's index.pug tempalte
 import * as fs from 'fs';
-fs.readdir('../templates', (err, files) => {
-    files.forEach(page => {
-        const path = `${__dirname}/../templates/${page}`;
-        if (!fs.lstatSync(path).isDirectory()) {
-            return;
+import * as util from 'util';
+
+const readdirPromise = util.promisify(fs.readdir);
+const statPromise = util.promisify(fs.stat);
+
+async function scanDirRecursive(dirPath: string, webPath = '/') {
+    // Make sure a file path is supplied
+    if (!dirPath) {
+        throw 'You must supply a filepath to begin the scan at';
+    }
+    // Read the directory
+    const files = await readdirPromise(dirPath);
+    for (const file of files) {
+        // Render the file if it's named 'index.pug'
+        if (file === 'index.pug') {
+            app.get(webPath, (req, res) => {
+                res.render(`${__dirname}/${dirPath}/${file}`);
+            });
         }
-        app.get(`/${page}`, (req, res) => {
-            res.render(`${path}/index`);
-        });
-    });
-});
+        else {
+            const stat = await statPromise(`${dirPath}/${file}`);
+            // Begin a recursive scan if the file is a directory
+            if (stat.isDirectory()) {
+                scanDirRecursive(`${dirPath}/${file}`, `${webPath}${file}/`);
+            }
+        }
+    }
+}
+scanDirRecursive('../templates');
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
